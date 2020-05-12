41d450c4110666c99d595946988ba9dd0a4f7a2f
V.Yaswanth Sai <50363676+Yaswanthsai28@users.noreply.github.com>
2020-05-12 23:54:47 +0530

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,52 @@
+//Data Types
+console.log('Im new to js');
+
+var name = 'yaswanth';
+
+console.log(name);
+
+var age = 20;
+
+console.log(age);
+
+var fullAge = true;
+
+console.log(fullAge);
+
+var salary;
+
+console.log(salary);
+
+//Type coercion
+
+console.log('My name is ' + name + ' and I am ' + age + ' year old'); 
+
+var job,single;
+
+job = 'student';
+single = true;
+
+console.log('is he single ? ' + single);
+
+salary = 0;
+
+console.log(salary + ' because im a ' + job);
+
+//Variable mutation
+
+age = 'Twenty';
+
+alert('My name is ' + name + ' and I am ' + age + ' year old');
+
+var lastName = prompt('what is your last name');
+
+console.log(name + ' ' + lastName);
+
+
+
+
+
+
+
+
+
